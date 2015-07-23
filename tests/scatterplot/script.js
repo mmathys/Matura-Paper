c1312b649bce599599362281a18aeea63ed6c849
Max Mathys <threadox@gmail.com>
2015-07-23 14:23:38 +0300

    mask problem resolved

--- tests/scatterplot/script.js
+++ tests/scatterplot/script.js
@@ -9,7 +9,7 @@ var yScale = d3.scale.linear();
 // Graphs
 var w = 850;
 var h = 400;
-var graphTransform = {xtop: 50, ytop: 0, xbottom:0, ybottom:50};
+var graphTransform = {xstart: 50, ytop: 0, xend:0, ybottom:50};
 
 // Global Zoomvariablen
 var transform = {x:0,y:0};
@@ -18,14 +18,19 @@ var scale = 1;
 // Wertebereich der Achsenskalierungen definieren. Hier ist die Anzahl der Pixel
 // gemeint, über die sich die Achsen erstrecken. Die x-Achse und die y-Achse
 // verschieben wir um 50 nach rechts, damit man die y-Achse beschriften kann.
-xScale.range([0,w-50]);
-yScale.range([h-50, 0]);
+xScale.range([0,w - graphTransform.xstart - graphTransform.xend]);
+yScale.range([h - graphTransform.ytop - graphTransform.ybottom, 0]);
 
 // Die Achsen werden von d3 generiert.
 var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
 var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(5);
 
-// Laden des Datensatzes, wird in 'data' als Array von d3 kopiert.
+/**
+ * Laden des Datensatzes durch d3, wird in den Array data geladen.
+ * @param  {[String]} 'data.csv'            Pfad der csv-Datei
+ * @param  {[Function]} function(err, data) callback-Funktion, mit Fehlerelement und
+ *                                			 		Datenarray
+ */
 d3.csv('data.csv', function(err, data) {
 
   // Schleife, um die Einträge zu formatieren
@@ -42,19 +47,21 @@ d3.csv('data.csv', function(err, data) {
     data[i].Mean = (data[i].Open + data[i].Close) / 2;
   }
 
-  // Wertebereich der Daten bestimmen mit d3: Um einen kleinen Abstand zwischen
-  // den maximalen Punkten und dem Ende des Rändern des Diagrammes zu bewahren,
-  // wird der Unterschied (Δ) des Minimums und dem untersuchten Wert mit 1.1
-  // mulitpliziert. Anschliessend wird die Summe des Minimums und des
-  // multiplizierten Wertes an d3 zurückgegeben.
+  /**
+   *  Wertebereich der Daten bestimmen mit d3: Um einen kleinen Abstand zwischen
+   *  den maximalen Punkten und dem Ende des Rändern des Diagrammes zu bewahren,
+   *  wird der Unterschied (Δ) des Minimums und dem untersuchten Wert mit 1.1
+   *  mulitpliziert. Anschliessend wird die Summe des Minimums und des
+   *  multiplizierten Wertes an d3 zurückgegeben.
+   */
   var xWertebereich = [];
   xWertebereich[0] = d3.min(data, function(d) {
     return d.Date;
   });
   xWertebereich[1] = d3.max(data, function(d) {
     var ΔDate = d.Date.getTime() - xWertebereich[0].getTime();
-    ΔDate*=1.1;
-    return new Date(ΔDate+xWertebereich[0].getTime());
+    ΔDate *= 1.1;
+    return new Date(ΔDate + xWertebereich[0].getTime());
   });
 
   var yWertebereich = [];
@@ -62,8 +69,8 @@ d3.csv('data.csv', function(err, data) {
     return d.Mean;
   });
   yWertebereich[1] = d3.max(data, function(d) {
-    var ΔMean = d.Mean-yWertebereich[0];
-    return yWertebereich[0]+ΔMean*1.1;
+    var ΔMean = d.Mean - yWertebereich[0];
+    return yWertebereich[0] + ΔMean * 1.1;
   });
 
   xScale.domain(xWertebereich);
@@ -112,16 +119,18 @@ d3.csv('data.csv', function(err, data) {
   v.append("mask")
     .attr("id", "mask")
     .append("rect")
-      .attr("x", graphTransform.xtop)
-      .attr("y", graphTransform.ytop)
-      .attr("width", (w-graphTransform.xtop-graphTransform.xbottom))
-      .attr("height", (h-graphTransform.ytop-graphTransform.ybottom))
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", w - graphTransform.xstart - graphTransform.xend)
+      .attr("height", h - graphTransform.ytop - graphTransform.ybottom)
       .attr("fill", "white");
 
   graph = v.append("g")
-    .attr("transform", "translate("+graphTransform.x+","+graphTransform.y+")")
+    .attr("id", "graph")
+    .attr("transform", "translate(" + graphTransform.xstart + "," + graphTransform.ytop + ")")
     .attr("mask", "url(#mask)")
     .selectAll("circle")
+
     // Die Daten zum Element mit der d3-Binding-Method binden: Die nach dem
     // enter() stehenden Befehle werden für alle Elemente in dem Array
     // ausgeführt.
@@ -134,12 +143,18 @@ d3.csv('data.csv', function(err, data) {
   // Achsen hinzufügen
   var xAxisContainer = v.append("g")
     .attr("class", "axis axis-x")
-    .attr("transform", "translate(50,"+(h-50)+")")
+    .attr("transform", "translate(" +
+      graphTransform.xstart + "," +
+      (h - graphTransform.ybottom) + ")")
     .call(xAxis);
 
   var yAxisContainer = v.append("g")
     .attr("class", "axis axis-y")
     .attr("transform", "translate(50,0)")
     .call(yAxis);
+
+  graph.on("mousemove", function(){
+    console.log("hi");
+  });
 
 });
