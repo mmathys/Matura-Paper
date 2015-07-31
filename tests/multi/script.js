17380df2dc2d0abec4452503627afa4041fd8602
Max Mathys <threadox@gmail.com>
2015-07-31 16:50:35 +0300

    multiple datasets added. Think of a better strategy than merging datasets.

--- tests/multi/script.js
+++ tests/multi/script.js
@@ -15,7 +15,7 @@ var points = require('./modules/points');
 
 // Für die Visualisation benötigte Variablen
 
-var config, index, values, v_accessor, v_acessor_scaled, v_accessor_cord, xScale, yScale, w, h, graphTransform, mouse,
+var config, files, index, values, v_accessor, v_acessor_scaled, v_accessor_cord, xScale, yScale, w, h, graphTransform, mouse,
   xAxis, yAxis, showPoints;
 
 showPoints = false;
@@ -33,11 +33,18 @@ d3.json("meta.json", function(err, res) {
     return;
   }
 
-  config = res;
+  config = res.config;
+  files = res.files;
 
-  //TODO: 1. Variablen global machen, die für den nächsten Teil benötigt werden✔︎
-  //      2. load() unkommentieren ✔︎
-  //      3. Die config auch wirklich anwenden
+  // TODO: Mehrere Files importieren können:
+  // Anforderungen: die 'index'- bzw. 'value'-Spalten müssen untereinander immer
+  //                den gleichen Datentyp und das gleiche Format haben.
+  // Die Tabellen werden 'gemergt', also wie untereinander angehängt. Will man
+  // eine Datenreihe in einer anderen Datei fortsetzen, muss die den gleichen
+  // Namen tragen.
+  //
+  // Die Config in Meta.json trifft auf beide Tabellen zu: Als Identifizierungs-
+  // zeichen wird das Attribut 'row' gebraucht.
 
   index = {};
   // Der Array der Datenreihen (Config).
@@ -50,9 +57,13 @@ d3.json("meta.json", function(err, res) {
       index = config[i];
     } else if(config[i].type == "value") {
       config[i].color = colors(values.length+1);
-      config[i].activated = true;
+      // Wenn das Attribut activated nicht gesetzt ist, setze es auch true.
+      if(typeof config[i].activated == 'undefined') {
+        config[i].activated = true;
+      }
       values.push(config[i]);
     }
+    // Bei unbekannten Typen: ignorieren.
   }
 
   if(index.data_type=="Number") {
@@ -128,7 +139,7 @@ d3.json("meta.json", function(err, res) {
    };
 
    // Die Visualisation laden
-   load();
+   loadFiles();
 });
 
 
@@ -140,7 +151,36 @@ d3.json("meta.json", function(err, res) {
  *
  ******************************************************************************/
 
-function load() {
+function loadFiles() {
+
+  // TODO: Don't merge.
+  /*
+  var loaded = 0;
+  var merge = [];
+
+  for(var i = 0; i<files.length; i++){
+    console.log(files[i]);
+    d3.csv(files[i], function(err, resp) {
+      if(err){
+        alert(err);
+        console.log(err);
+        return;
+      }
+
+
+      for(var j = 0; j<resp.length; j++){
+        merge.push(resp[j]);
+      }
+
+      if(++loaded == files.length){
+        console.log("loaded");
+        //loadVisualization(merge);
+      }
+
+    });
+
+  }
+  */
 
   /**
    * Laden des Datensatzes durch d3, wird in den Array data geladen.
@@ -150,240 +190,245 @@ function load() {
    */
   d3.csv('data.csv', function(err, data) {
 
-    /**
-     *
-     * Formatieren des Datensatzes
-     *
-     */
+    loadVisualization(data);
 
-    // Sortieren, denn wir brauchen dies für unseren Tooltip-Algorithmus
-    data = sort(data, index);
+  });
+}
 
-    // Schleife, um die Einträge zu formatieren: Strings in ein Javascript-
-    // Objekt konvertieren.
-    for(var i = 0; i<data.length; i++) {
+function loadVisualization(data) {
+  /**
+   *
+   * Formatieren des Datensatzes
+   *
+   */
 
-      for(var j = 0; j<config.length; j++) {
-        if(config[j].data_type == "Number") {
-          data[i][config[j].row] = parseFloat(data[i][config[j].row]);
-        } else if(config[j].data_type == "Date") {
-          data[i][config[j].row] =  d3.time.format(config[j].date_format)
-                                      .parse(data[i][config[j].row]);
-        }
+  // Sortieren, denn wir brauchen dies für unseren Tooltip-Algorithmus
+  data = sort(data, index);
+
+  // Schleife, um die Einträge zu formatieren: Strings in ein Javascript-
+  // Objekt konvertieren.
+  for(var i = 0; i<data.length; i++) {
+
+    for(var j = 0; j<config.length; j++) {
+      if(config[j].data_type == "Number") {
+        data[i][config[j].row] = parseFloat(data[i][config[j].row]);
+      } else if(config[j].data_type == "Date") {
+        data[i][config[j].row] =  d3.time.format(config[j].date_format)
+                                    .parse(data[i][config[j].row]);
       }
-
     }
 
-    /**
-     *
-     * d3-Achsen initalisieren
-     *
-     */
+  }
 
-    /**
-     *  Wertebereich der Daten bestimmen mit d3: Um einen kleinen Abstand zwischen
-     *  den maximalen Punkten und dem Ende des Rändern des Diagrammes zu bewahren,
-     *  wird der Unterschied (Δ) des Minimums und dem untersuchten Wert mit 1.1
-     *  mulitpliziert. Anschliessend wird die Summe des Minimums und des
-     *  multiplizierten Wertes an d3 zurückgegeben.
-     */
-    var xWertebereich = [];
-    var yWertebereich = [];
+  /**
+   *
+   * d3-Achsen initalisieren
+   *
+   */
 
-    xWertebereich[0] = range.min(data, index.accessor);
-    xWertebereich[1] = range.max(data, index.accessor);
+  /**
+   *  Wertebereich der Daten bestimmen mit d3: Um einen kleinen Abstand zwischen
+   *  den maximalen Punkten und dem Ende des Rändern des Diagrammes zu bewahren,
+   *  wird der Unterschied (Δ) des Minimums und dem untersuchten Wert mit 1.1
+   *  mulitpliziert. Anschliessend wird die Summe des Minimums und des
+   *  multiplizierten Wertes an d3 zurückgegeben.
+   */
+  var xWertebereich = [];
+  var yWertebereich = [];
 
-    yWertebereich[0] = range.minMultipleSets(data, values, v_accessor);
-    yWertebereich[1] = range.maxMultipleSets(data, values, v_accessor);
+  xWertebereich[0] = range.min(data, index.accessor);
+  xWertebereich[1] = range.max(data, index.accessor);
 
-    console.log(yWertebereich);
+  yWertebereich[0] = range.minMultipleSets(data, values, v_accessor);
+  yWertebereich[1] = range.maxMultipleSets(data, values, v_accessor);
 
-    xWertebereich[1] = range.applyOverflow(xWertebereich[0], xWertebereich[1],
-      1.1, index.data_type);
-    yWertebereich[1] = range.applyOverflow(yWertebereich[0], yWertebereich[1],
-      1.1, values[0].data_type);
+  console.log(yWertebereich);
 
-    xScale.domain(xWertebereich);
-    yScale.domain(yWertebereich);
+  xWertebereich[1] = range.applyOverflow(xWertebereich[0], xWertebereich[1],
+    1.1, index.data_type);
+  yWertebereich[1] = range.applyOverflow(yWertebereich[0], yWertebereich[1],
+    1.1, values[0].data_type);
 
-    /**
-     *
-     * Zoom
-     *
-     */
+  xScale.domain(xWertebereich);
+  yScale.domain(yWertebereich);
 
-    // Zoom hinzufügen, das durch d3 unterstützt wird
-    var zoom = d3.behavior.zoom()
-      .x(xScale)
-      .y(yScale)
-      .scaleExtent([1, 50])
-      .on("zoom", zoomed);
+  /**
+   *
+   * Zoom
+   *
+   */
 
-    // die variable graph initialiseren, damit sie in der Funktion zoomed() ver-
-    // wendet werden kann, obwohl sie erst später definiert wird.
-    var graph = null;
+  // Zoom hinzufügen, das durch d3 unterstützt wird
+  var zoom = d3.behavior.zoom()
+    .x(xScale)
+    .y(yScale)
+    .scaleExtent([1, 50])
+    .on("zoom", zoomed);
 
-    // Mit der Funktion 'zoomed' werden die x-Achse und die y-Achse aktualisiert
-    function zoomed() {
-      // Achsen neu zeichnen
-      xAxisContainer.call(xAxis);
-      yAxisContainer.call(yAxis);
+  // die variable graph initialiseren, damit sie in der Funktion zoomed() ver-
+  // wendet werden kann, obwohl sie erst später definiert wird.
+  var graph = null;
 
-      // Punkte neu berechnen.
-      for(var i = 0; i<values.length; i++) {
-        v.selectAll("circle.data-point[data-row='" + values[i].row + "']")
-          .attr("cx", index.accessor_scaled)
-          .attr("cy", v_accessor_scaled(values[i]));
-      }
+  // Mit der Funktion 'zoomed' werden die x-Achse und die y-Achse aktualisiert
+  function zoomed() {
+    // Achsen neu zeichnen
+    xAxisContainer.call(xAxis);
+    yAxisContainer.call(yAxis);
 
-      // Tooltip bei Zoom auch aktualisieren
-      tooltip.updateTooltip(data, xScale, yScale, index, values, v_accessor, v_accessor_scaled, v_accessor_cord);
-
-      // Linie bei Zoom aktualisieren
-      line.update(data, index, values, v_accessor_scaled, v_accessor_cord);
-    }
-
-    /**
-     *
-     * (Datengebundene) Elemente einfügen
-     *
-     */
-
-    // SVG-Element mit id 'visualization' extrahieren
-    var v = d3.select("#visualization")
-      .attr("width", w)
-      .attr("height", h)
-
-    // Unterstützung für Zoom hinzufügen (d3)
-      .call(zoom);
-
-    // SVG-Maske für den Graph: Wir wollen nicht, dass Punkte aus unserem
-    // definierten Feld auftauchen.
-    v.append("mask")
-      .attr("id", "mask")
-      .append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", w - graphTransform.xstart - graphTransform.xend)
-        .attr("height", h - graphTransform.ytop - graphTransform.ybottom)
-        .attr("fill", "white");
-
-    graph = v.append("g")
-      .attr("id", "graph")
-      .attr("transform", "translate(" + graphTransform.xstart +
-        "," + graphTransform.ytop + ")")
-      .attr("mask", "url(#mask)");
-
-    // Die Daten zum Element mit der d3-Binding-Method binden: Die nach dem
-    // enter() stehenden Befehle werden für alle Elemente in dem Array
-    // ausgeführt.
-    var circles = graph.selectAll("circle")
-      .data(data).enter();
-
+    // Punkte neu berechnen.
     for(var i = 0; i<values.length; i++) {
-      circles.append("circle")
-          .attr("class", "data-point")
-          .attr("data-row", values[i].row)
-          //TODO: add here attribute data-row and add support in the whole code
-          // for this.
-          .attr("cx", index.accessor_scaled)
-          .attr("cy", v_accessor_scaled(values[i]));
+      v.selectAll("circle.data-point[data-row='" + values[i].row + "']")
+        .attr("cx", index.accessor_scaled)
+        .attr("cy", v_accessor_scaled(values[i]));
     }
 
-    // Sichtbarkeit prüfen
+    // Tooltip bei Zoom auch aktualisieren
+    tooltip.updateTooltip(data, xScale, yScale, index, values, v_accessor, v_accessor_scaled, v_accessor_cord);
 
-    points.updateVisibility();
+    // Linie bei Zoom aktualisieren
+    line.update(data, index, values, v_accessor_scaled, v_accessor_cord);
+  }
 
-    /**
-     *
-     * d3-Achsen einfügen
-     *
-     */
+  /**
+   *
+   * (Datengebundene) Elemente einfügen
+   *
+   */
 
-    // Achsen hinzufügen
-    var xAxisContainer = v.append("g")
-      .attr("class", "axis axis-x")
-      .attr("transform", "translate(" +
-        graphTransform.xstart + "," +
-        (h - graphTransform.ybottom) + ")")
-      .call(xAxis);
+  // SVG-Element mit id 'visualization' extrahieren
+  var v = d3.select("#visualization")
+    .attr("width", w)
+    .attr("height", h)
 
-    var yAxisContainer = v.append("g")
-      .attr("class", "axis axis-y")
-      .attr("transform", "translate(50,0)")
-      .call(yAxis);
+  // Unterstützung für Zoom hinzufügen (d3)
+    .call(zoom);
 
-    /**
-     *
-     * Tooltip (nicht von d3)
-     *
-     */
-
-    // Maus-Koordinaten: Um auf die Maus-Koordinaten zugreifen zu können, muss man
-    // ein unsichtbares Element über den gesamten Graph legen, der alle
-    // 'Maus-Events' "aufnimmt". Ein leerer g-SVG-Container (wie 'graph') ist
-    // nicht fähig, Maus-Events aufzunehmen.
-    v.append("rect")
-      .attr("id", "overlay")
-      .attr("x", graphTransform.xstart)
-      .attr("y", graphTransform.ytop)
+  // SVG-Maske für den Graph: Wir wollen nicht, dass Punkte aus unserem
+  // definierten Feld auftauchen.
+  v.append("mask")
+    .attr("id", "mask")
+    .append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
       .attr("width", w - graphTransform.xstart - graphTransform.xend)
       .attr("height", h - graphTransform.ytop - graphTransform.ybottom)
-      .on("mousemove", function() {
-        tooltip.mouse = d3.mouse(this);
-        tooltip.updateTooltip(data, xScale, yScale, index, values, v_accessor, v_accessor_scaled, v_accessor_cord);
-      });
+      .attr("fill", "white");
 
-    /**
-     *
-     * Linien
-     *
-     */
+  graph = v.append("g")
+    .attr("id", "graph")
+    .attr("transform", "translate(" + graphTransform.xstart +
+      "," + graphTransform.ytop + ")")
+    .attr("mask", "url(#mask)");
 
-     for(var i = 0; i<values.length; i++) {
-        line.addLine(index, values[i], data, v_accessor_cord(values[i].row));
-     }
+  // Die Daten zum Element mit der d3-Binding-Method binden: Die nach dem
+  // enter() stehenden Befehle werden für alle Elemente in dem Array
+  // ausgeführt.
+  var circles = graph.selectAll("circle")
+    .data(data).enter();
 
-     $('select').on('change', function() {
-       line.mode = this.value;
-       line.update(data, index, values, v_accessor_scaled, v_accessor_cord);
-     });
+  for(var i = 0; i<values.length; i++) {
+    circles.append("circle")
+        .attr("class", "data-point")
+        .attr("data-row", values[i].row)
+        //TODO: add here attribute data-row and add support in the whole code
+        // for this.
+        .attr("cx", index.accessor_scaled)
+        .attr("cy", v_accessor_scaled(values[i]));
+  }
 
-     $('#checkbox').on('change', function() {
-       if($(this).is(":checked")){
-         showPoints = true;
-       } else {
-         showPoints = false;
-        }
-        points.visible = showPoints;
-        points.updateVisibility();
-     });
+  // Sichtbarkeit prüfen
 
-     /**
-      * Toggles
-      */
+  points.updateVisibility();
 
-      for(var i = 0; i<values.length; i++){
-        d3.select("#select-row")
-          .append("p")
-          .attr("class", "select-row-item")
-          .attr("style", "border-color:"+values[i].color)
-          .attr("data-row", values[i].row)
-          .text(values[i].name ? values[i].name : values[i].row);
+  /**
+   *
+   * d3-Achsen einfügen
+   *
+   */
 
-        $(".select-row-item[data-row='" + values[i].row + "']").on('click', function() {
-          var row = $(this).attr("data-row");
+  // Achsen hinzufügen
+  var xAxisContainer = v.append("g")
+    .attr("class", "axis axis-x")
+    .attr("transform", "translate(" +
+      graphTransform.xstart + "," +
+      (h - graphTransform.ybottom) + ")")
+    .call(xAxis);
 
-          if($(this).hasClass("inactive")){
-            // activate this
-            $(this).toggleClass("inactive", false);
-            line.setActivated(true, row, values);
-          } else {
-            // deactivate this
-            $(this).toggleClass("inactive", true);
-            line.setActivated(false, row, values);
-          }
-        });
+  var yAxisContainer = v.append("g")
+    .attr("class", "axis axis-y")
+    .attr("transform", "translate(50,0)")
+    .call(yAxis);
+
+  /**
+   *
+   * Tooltip (nicht von d3)
+   *
+   */
+
+  // Maus-Koordinaten: Um auf die Maus-Koordinaten zugreifen zu können, muss man
+  // ein unsichtbares Element über den gesamten Graph legen, der alle
+  // 'Maus-Events' "aufnimmt". Ein leerer g-SVG-Container (wie 'graph') ist
+  // nicht fähig, Maus-Events aufzunehmen.
+  v.append("rect")
+    .attr("id", "overlay")
+    .attr("x", graphTransform.xstart)
+    .attr("y", graphTransform.ytop)
+    .attr("width", w - graphTransform.xstart - graphTransform.xend)
+    .attr("height", h - graphTransform.ytop - graphTransform.ybottom)
+    .on("mousemove", function() {
+      tooltip.mouse = d3.mouse(this);
+      tooltip.updateTooltip(data, xScale, yScale, index, values, v_accessor, v_accessor_scaled, v_accessor_cord);
+    });
+
+  /**
+   *
+   * Linien
+   *
+   */
+
+   for(var i = 0; i<values.length; i++) {
+      line.addLine(index, values[i], data, v_accessor_cord(values[i].row));
+   }
+
+   $('select').on('change', function() {
+     line.mode = this.value;
+     line.update(data, index, values, v_accessor_scaled, v_accessor_cord);
+   });
+
+   $('#checkbox').on('change', function() {
+     if($(this).is(":checked")){
+       showPoints = true;
+     } else {
+       showPoints = false;
       }
-  });
+      points.visible = showPoints;
+      points.updateVisibility();
+   });
+
+   /**
+    * Toggles
+    */
+
+    for(var i = 0; i<values.length; i++){
+      d3.select("#select-row")
+        .append("p")
+        .attr("class", "select-row-item")
+        .attr("style", "border-color:"+values[i].color)
+        .attr("data-row", values[i].row)
+        .text(values[i].name ? values[i].name : values[i].row);
+
+      $(".select-row-item[data-row='" + values[i].row + "']").on('click', function() {
+        var row = $(this).attr("data-row");
+
+        if($(this).hasClass("inactive")){
+          // activate this
+          $(this).toggleClass("inactive", false);
+          line.setActivated(true, row, values);
+        } else {
+          // deactivate this
+          $(this).toggleClass("inactive", true);
+          line.setActivated(false, row, values);
+        }
+      });
+    }
 }
