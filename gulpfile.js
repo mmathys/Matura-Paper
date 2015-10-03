46bb3e3cc5433258981f56b1e40276ef4ba0cf3f
Max Mathys <threadox@gmail.com>
2015-10-03 16:59:57 +0200

    keine lint mehr, standard js syntax implementiert

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,58 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var concat = require('gulp-concat')
+var jshint = require('gulp-jshint')
 var bs = require('browser-sync').create()
 
 // bundling
-var browserify = require('browserify');
-var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
-var source = require('vinyl-source-stream');
-var glob = require('glob');
-var path = require('path');
+var browserify = require('browserify')
+var buffer = require('vinyl-buffer')
+var sourcemaps = require('gulp-sourcemaps')
+var gutil = require('gulp-util')
+var uglify = require('gulp-uglify')
+var source = require('vinyl-source-stream')
+var glob = require('glob')
+var path = require('path')
 
-gulp.task('init', ['libs']);
-gulp.task('build', ['css', 'files', 'js', 'lint', 'webserver']);
-gulp.task('develop', ['build', 'watch']);
+gulp.task('init', ['libs'])
+gulp.task('build', ['css', 'files', 'js', 'webserver'])
+gulp.task('develop', ['build', 'watch'])
 
-gulp.task('watch', ['files', 'lint', 'js', 'css'], function() {
-  gulp.watch(['./common/scss/*.scss', './tests/**/*.scss'], ['css']);
-  gulp.watch(['./tests/**/*.{html,png,jpg,jpeg,svg,csv,json,txt}'], ['files']);
-  gulp.watch(['./tests/**/*.js'], ['js', 'lint']);
-});
-
-gulp.task('refresh', function() {
-  bs.refresh();
+gulp.task('watch', ['files', 'js', 'css'], function () {
+  gulp.watch(['./common/scss/*.scss', './tests/**/*.scss'], ['css'])
+  gulp.watch(['./tests/**/*.{html,png,jpg,jpeg,svg,csv,json,txt}'], ['files'])
+  gulp.watch(['./tests/**/*.js'], ['js'])
 })
 
-gulp.task('css', function() {
+gulp.task('refresh', function () {
+  bs.refresh()
+})
 
+gulp.task('css', function () {
   gulp.src(['./common/scss/*.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('style.css'))
     .pipe(gulp.dest('./dist/css'))
-    .pipe(bs.stream());
+    .pipe(bs.stream())
 
   gulp.src(['./tests/**/*.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'))
-    .pipe(bs.stream());
-});
+    .pipe(bs.stream())
+})
 
-gulp.task('js', function() {
-
-  glob('./tests/**/script.js', function(err, files) {
-    files.map(function(file) {
-      var testName = "";
-      var arr = path.join(file, "..").split(path.sep);
-      testName = arr[arr.length-1];
+gulp.task('js', function () {
+  glob('./tests/**/script.js', function (err, files) {
+    files.map(function (file) {
+      var testName = ''
+      var arr = path.join(file, '..').split(path.sep)
+      testName = arr[arr.length - 1]
 
       // Browserify
       var b = browserify({
         entries: file,
         debug: true
-      });
+      })
 
       return b.bundle()
         .pipe(source('script.js'))
@@ -66,15 +64,19 @@ gulp.task('js', function() {
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(path.join('./dist', testName)))
         .pipe(bs.stream())
-    });
-  });
-});
+    })
+  })
+})
 
-gulp.task('lint', function() {
-  gulp.src(['./tests/**/script.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-});
+
+
+//gulp.task('lint', function() {
+//  gulp.src(['./tests/**/script.js'])
+//    .pipe(jshint())
+//    .pipe(jshint.reporter('jshint-stylish'));
+//});
+
+
 
 gulp.task('libs', function() {
   gulp.src('common/lib/*.js')
@@ -87,7 +89,7 @@ gulp.task('files', function() {
     .pipe(bs.stream())
 });
 
-gulp.task('webserver', ['files', 'lint', 'js', 'css'], function() {
+gulp.task('webserver', ['files', 'js', 'css'], function() {
     bs.init({
         server: {
             baseDir: "./dist"
